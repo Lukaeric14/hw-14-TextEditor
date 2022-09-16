8a8ef2c8d82f90bc15e973d1e0bf525083fe42b8
lukaeric14 <lukaericnu@gmail.com>
2022-09-16 13:13:11 +0530

    feat: add func to put and get from indexDB

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,43 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("putting to db");
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const db = await openDB("jate", 1);
+  const transaction = db.transaction("jate", "readwrite");
+  const store = transaction.objectStore("jate");
+  const res = store.put({ id: 1, value: content });
+
+  console.log("data saved to db", res.value);
+};
+
+export const getDb = async () => {
+  console.log("getting from db");
+
+  const db = await openDB("jate", 1);
+  const transaction = db.transaction("jate", "readonly");
+  const store = transaction.objectStore("jate");
+  const res = await store.get(1);
+
+  if (res) {
+    console.log("Retrieved Data: ", res.value);
+    return res.value;
+  } else if (!result) {
+    console.log("No Data found in db");
+    return;
+  }
+};
 
 initdb();
